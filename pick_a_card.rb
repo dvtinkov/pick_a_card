326161a4f88e83b2bed604a032415bf43e2c38d3
dvtinkov <93160604+dvtinkov@users.noreply.github.com>
2021-10-26 23:18:01 +0300

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,6 +1,6 @@
 #encoding: utf-8
 
 values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ['\u2660', '\u2665', '\u2665', '\u2663']
 
 puts "#{values.sample} of #{suits.sample}"
